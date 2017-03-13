5b73a1f51900746e43fc937289b81a090a39d6ec
Will Kearney <wkearn@gmail.com>
2017-03-13 16:58:04 -0400

    Use the new ADCPDataProcessing package

--- src/TidalDischargeModels.jl
+++ src/TidalDischargeModels.jl
@@ -2,21 +2,14 @@ module TidalDischargeModels
 
 using Reexport
 
-@reexport using Base.Dates, StatsBase, Optim, DischargeData
+@reexport using Base.Dates, StatsBase, Optim, DischargeData, ADCPDataProcessing
 
-include("Databases.jl")
-
-include("ADCPTypes.jl") # A variety of types for ADCP deployments
-include("ADCPDataStructures.jl") # Types to hold loaded ADCP data
 include("MetData.jl") # Types to hold meteorological data for atmospheric correction
 include("DischargeDataStructures.jl") # Process ADCP data into discharges
 include("Calibrations.jl") # Calibrate ADCP discharges to true discharges
 
-@reexport using TidalDischargeModels.ADCPTypes,
-TidalDischargeModels.ADCPDataStructures,
-TidalDischargeModels.MetData,
+@reexport using TidalDischargeModels.MetData,
 TidalDischargeModels.DischargeDataStructures,
-TidalDischargeModels.Databases,
 TidalDischargeModels.Calibrations
 
 export estfun, estfun!, evalfun, evalmodel, BoonModel, LTIModel, RegularizedLTIModel, VolterraModel, RegularizedVolterraModel, kMeansModel, ThresholdModel, nash_sutcliffe, flatness, ρ, c_p
