8d3529a29ed990ae0d0eadaeaba210c7779d40b3
William Kearney <wkearn@gmail.com>
2017-03-14 16:20:01 -0400

    Get rid of DDS and MD in favor of ADP
    This should complete the data processing
    package spinoffs. All that remains here is Calibrations.jl

--- src/TidalDischargeModels.jl
+++ src/TidalDischargeModels.jl
@@ -4,13 +4,9 @@ using Reexport
 
 @reexport using Base.Dates, StatsBase, Optim, DischargeData, ADCPDataProcessing
 
-include("MetData.jl") # Types to hold meteorological data for atmospheric correction
-include("DischargeDataStructures.jl") # Process ADCP data into discharges
 include("Calibrations.jl") # Calibrate ADCP discharges to true discharges
 
-@reexport using TidalDischargeModels.MetData,
-TidalDischargeModels.DischargeDataStructures,
-TidalDischargeModels.Calibrations
+@reexport using TidalDischargeModels.Calibrations
 
 export estfun, estfun!, evalfun, evalmodel, BoonModel, LTIModel, RegularizedLTIModel, VolterraModel, RegularizedVolterraModel, kMeansModel, ThresholdModel, nash_sutcliffe, flatness, ρ, c_p
 
