ad7d36078dbd299b828f5df38e0f9837551a3a8d
Will Kearney <wkearn@gmail.com>
2017-04-06 17:19:25 -0400

    validate and evalmodel with the new quantities

--- test/summarystats.jl
+++ test/summarystats.jl
@@ -1,6 +1,6 @@
 println("Calculating summary statistics")
 for m in [boon,lti,volterra,kmm]
-    err = msd(TDM.validate(Q2.Q,1:N2,m.M),TDM.evalmodel(m,Q2,1:N2))
+    err = msd(TDM.validate(quantity(Q2),1:N2,m.M),TDM.evalmodel(m,p2,Q2,1:N2))
     ns = nash_sutcliffe(m,Q2)
     sf = flatness(m,Q2)
     println(typeof(m),":: ",err,"\t",ns,"\t",sf)
