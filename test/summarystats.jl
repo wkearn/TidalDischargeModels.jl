d7e283e61131729ce369c84055c3e83cb347d697
Will Kearney <wkearn@gmail.com>
2017-10-18 12:35:39 -0400

    Qualifying msd

--- test/summarystats.jl
+++ test/summarystats.jl
@@ -1,6 +1,6 @@
 println("Calculating summary statistics")
 for m in [boon,lti,volterra,kmm]
-    err = msd(TDM.validate(quantity(Q2),1:N2,m.M),TDM.evalmodel(m,p2,Q2,1:N2))
+    err = StatsBase.msd(TDM.validate(quantity(Q2),1:N2,m.M),TDM.evalmodel(m,p2,Q2,1:N2))
     ns = nash_sutcliffe(m,p2,Q2)
     sf = flatness(m,p2,Q2)
     println(typeof(m),":: ",err,"\t",ns,"\t",sf)
