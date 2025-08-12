0c9bf802c829521c92bf12e2537011a7660bc762
William Kearney <will@wskearney.com>
2025-08-12 12:02:00 +0200

    Update testing infrastructure (#31)

--- test/data/data.jl
+++ test/data/data.jl
@@ -1,39 +1,40 @@
-using TidalDischargeModels
-using Test
+using CSV, DataFrames, TidalDischargeModels
 
-using CSV, DataFrames
-
-
-# Load data from USGS RDB format
-stage_data = CSV.read(joinpath(@__DIR__, "data", "mallard_stage.rdb"), DataFrame,
+stage_data = CSV.read(joinpath(@__DIR__, "mallard_stage.rdb"), DataFrame,
                       comment="#", delim='\t',
                       dateformat="yyyy-mm-dd HH:MM",
                       header=["Agency", "Site", "DateTime", "TZ", "Stage", "Quality"],
                       skipto=29)
 
-discharge_data = CSV.read(joinpath(@__DIR__, "data", "mallard_discharge.rdb"), DataFrame,
+discharge_data = CSV.read(joinpath(@__DIR__, "mallard_discharge.rdb"), DataFrame,
                           comment="#", delim='\t',
                           header=["Agency", "Site", "DateTime", "TZ", "Discharge", "Quality"],
                           dateformat="yyyy-mm-dd HH:MM",
                           skipto=29)
 
-stage_test_data = CSV.read(joinpath(@__DIR__, "data", "mallard_test_stage.rdb"), DataFrame,
+stage_test_data = CSV.read(joinpath(@__DIR__, "mallard_test_stage.rdb"), DataFrame,
                       comment="#", delim='\t',
                       dateformat="yyyy-mm-dd HH:MM",
                       header=["Agency", "Site", "DateTime", "TZ", "Stage", "Quality"],
                       skipto=29)
 
-discharge_test_data = CSV.read(joinpath(@__DIR__, "data", "mallard_test_discharge.rdb"), DataFrame,
+discharge_test_data = CSV.read(joinpath(@__DIR__, "mallard_test_discharge.rdb"), DataFrame,
                           comment="#", delim='\t',
                           header=["Agency", "Site", "DateTime", "TZ", "Discharge", "Quality"],
                           dateformat="yyyy-mm-dd HH:MM",
                           skipto=29)
 
-# Join stage and discharge tables together so they are easier to
-# manage
 data = innerjoin(stage_data, discharge_data, on=[:Agency, :Site, :DateTime, :TZ], makeunique=true)
+
 test_data = innerjoin(stage_test_data, discharge_test_data, on=[:Agency, :Site, :DateTime, :TZ], makeunique=true)
 
-include("kmeans.jl")
+m = kMeansModel(3, 0, 27, 0.0)
+m = estfun(m, data[!,:Stage], data[!,:Discharge])
 
-                                      
+Qpp = evalmodel(m, test_data[!,:Stage], test_data[!,:Discharge])
+
+evalfun(m, test_data[!,:Stage], test_data[!,:Discharge])
+
+
+
+
