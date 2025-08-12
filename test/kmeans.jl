2b56776f7f10d320a5b13d6614216171ee65414b
William Kearney <will@wskearney.com>
2025-08-12 15:29:11 +0200

    Use the more consistent interface for kmeans model

--- test/kmeans.jl
+++ test/kmeans.jl
@@ -1,8 +1,6 @@
-# Fit the k means model
 m = kMeansModel(3, 0, 27, 0.0)
-m = estfun(m, data[!,:Stage], data[!,:Discharge])
+m = fit(m, data[!,:Stage], data[!,:Discharge])
+Qpp = predict(m, test_data[!, :Stage])
 
-# Evaluate the model on the test data
-Qpp = predict(m, test_data[!,:Stage])
-
-evalfun(m, test_data[!,:Stage], test_data[!,:Discharge])
+@test length(Qpp) == length(test_data[!, :Stage])
+@test count(ismissing, Qpp) == 2
