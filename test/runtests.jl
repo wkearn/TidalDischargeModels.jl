6d66418b04f3decc415e211677077468a0094995
William Kearney <will@wskearney.com>
2025-08-12 14:38:56 +0200

    Move test data loading to its own file

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,39 +1,7 @@
 using TidalDischargeModels
 using Test
 
-using CSV, DataFrames
-
-
-# Load data from USGS RDB format
-stage_data = CSV.read(joinpath(@__DIR__, "data", "mallard_stage.rdb"), DataFrame,
-                      comment="#", delim='\t',
-                      dateformat="yyyy-mm-dd HH:MM",
-                      header=["Agency", "Site", "DateTime", "TZ", "Stage", "Quality"],
-                      skipto=29)
-
-discharge_data = CSV.read(joinpath(@__DIR__, "data", "mallard_discharge.rdb"), DataFrame,
-                          comment="#", delim='\t',
-                          header=["Agency", "Site", "DateTime", "TZ", "Discharge", "Quality"],
-                          dateformat="yyyy-mm-dd HH:MM",
-                          skipto=29)
-
-stage_test_data = CSV.read(joinpath(@__DIR__, "data", "mallard_test_stage.rdb"), DataFrame,
-                      comment="#", delim='\t',
-                      dateformat="yyyy-mm-dd HH:MM",
-                      header=["Agency", "Site", "DateTime", "TZ", "Stage", "Quality"],
-                      skipto=29)
-
-discharge_test_data = CSV.read(joinpath(@__DIR__, "data", "mallard_test_discharge.rdb"), DataFrame,
-                          comment="#", delim='\t',
-                          header=["Agency", "Site", "DateTime", "TZ", "Discharge", "Quality"],
-                          dateformat="yyyy-mm-dd HH:MM",
-                          skipto=29)
-
-# Join stage and discharge tables together so they are easier to
-# manage
-data = innerjoin(stage_data, discharge_data, on=[:Agency, :Site, :DateTime, :TZ], makeunique=true)
-test_data = innerjoin(stage_test_data, discharge_test_data, on=[:Agency, :Site, :DateTime, :TZ], makeunique=true)
-
+include("data.jl")
 include("kmeans.jl")
 
                                       
