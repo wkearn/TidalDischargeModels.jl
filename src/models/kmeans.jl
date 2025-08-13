6b1ea072695552cfabec7b59f6af93ac328c45b8
William Kearney <will@wskearney.com>
2025-08-13 08:33:24 +0200

    Update fit methods to use ::Type{M} interface (#39)
    This avoids the awkward step of constructing a model object before it
    is fitted.

--- src/models/kmeans.jl
+++ src/models/kmeans.jl
@@ -10,8 +10,6 @@ struct kMeansModel <: DischargeModel
     Q::Vector{Float64}
 end
 
-kMeansModel(M,N,k=1,λ=0.0) = kMeansModel(M,k,λ,zeros(M,k),zeros(M,k),zeros(M,N),zeros(N))
-
 function clusters(M::kMeansModel,H)
     D = pairwise(Euclidean(),H,M.centers, dims=2)
     map(x->x[2], findmin(D, dims=2)[2])
@@ -28,12 +26,20 @@ function StatsBase.fit(M::kMeansModel,H::Matrix{Float64},Q::Vector{Float64})
     kMeansModel(M.M,M.k,M.λ,km.centers,β,H,Q)
 end
 
-function StatsBase.fit(m::kMeansModel, h::Vector{Float64}, Q::Vector{Float64})
-    H = zeros(m.M, length(h) - m.M + 1)
-    for i in 1:m.M
-        H[m.M - i + 1, :] = h[i:end - m.M + i]
+function StatsBase.fit(::Type{kMeansModel}, h::Vector{Float64}, Q::Vector{Float64}; M=1, k=1, λ=0.0)
+    H = zeros(M, length(h) - M + 1)
+    for i in 1:M
+        H[M - i + 1, :] = h[i:end - M + i]
     end
-    fit(m, H, Q[m.M:end])
+    Qt = Q[M:end]
+    km = kmeans(H,k,init=:kmcen,tol=1e-32)
+    β = zeros(M,k)
+    for i in 1:k
+        Hi = H[:,assignments(km).==i]
+        Qi = Qt[assignments(km).==i]
+        β[:,i] = (Hi*Hi'+λ*I)\(Hi*Qi)
+    end
+    kMeansModel(M,k,λ,km.centers,β,H,Qt)
 end
 
 function StatsBase.fit!(M::kMeansModel,H::Matrix{Float64},Q::Vector{Float64})
